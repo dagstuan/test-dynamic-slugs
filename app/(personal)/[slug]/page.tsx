44923843994322d76c03425c776930e02d1d581f
Dag Stuan <dag.stuan@bekk.no>
2025-03-29 15:46:00 +0100

    Try dynamic import page.

--- app/(personal)/[slug]/page.tsx
+++ app/(personal)/[slug]/page.tsx
@@ -1,11 +1,10 @@
-import {CustomPortableText} from '@/components/CustomPortableText'
-import {Header} from '@/components/Header'
 import {sanityFetch} from '@/sanity/lib/live'
 import {pagesBySlugQuery, slugsByTypeQuery} from '@/sanity/lib/queries'
 import type {Metadata, ResolvingMetadata} from 'next'
-import {toPlainText, type PortableTextBlock} from 'next-sanity'
-import {draftMode} from 'next/headers'
-import {notFound} from 'next/navigation'
+import {toPlainText} from 'next-sanity'
+import dynamic from 'next/dynamic'
+
+const PageContent = dynamic(() => import('./pageContent').then((mod) => mod.PageContent))
 
 type Props = {
   params: Promise<{slug: string}>
@@ -38,39 +37,5 @@ export async function generateStaticParams() {
 }
 
 export default async function PageSlugRoute({params}: Props) {
-  const {data} = await sanityFetch({query: pagesBySlugQuery, params})
-
-  // Only show the 404 page if we're in production, when in draft mode we might be about to create a page on this slug, and live reload won't work on the 404 route
-  if (!data?._id && !(await draftMode()).isEnabled) {
-    notFound()
-  }
-
-  const {body, overview, title} = data ?? {}
-
-  return (
-    <div>
-      <div className="mb-14">
-        {/* Header */}
-        <Header
-          id={data?._id || null}
-          type={data?._type || null}
-          path={['overview']}
-          title={title || (data?._id ? 'Untitled' : '404 Page Not Found')}
-          description={overview}
-        />
-
-        {/* Body */}
-        {body && (
-          <CustomPortableText
-            id={data?._id || null}
-            type={data?._type || null}
-            path={['body']}
-            paragraphClasses="font-serif max-w-3xl text-gray-600 text-xl"
-            value={body as unknown as PortableTextBlock[]}
-          />
-        )}
-      </div>
-      <div className="absolute left-0 w-screen border-t" />
-    </div>
-  )
+  return <PageContent params={params} />
 }
